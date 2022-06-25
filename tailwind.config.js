0b74ec0e3c1277feda6a6fa8c0ed08323dd694d9
Laitas <marciukash@gmail.com>
2022-06-25 11:47:41 +0300

    typeography setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "purple-1": "#AD1FEA",
+        "blue-1": "#4661E6",
+        "dark-accent": "#373F68",
+        "gray-1": "#F2F4FF",
+        "light-gray": "F7F8FD",
+        "light-accent": "#3A4374",
+        "dark-gray": "#647196",
+        "orange-1": "#F49F85",
+        "light-blue": "#62BCFA",
+      },
+    },
   },
   plugins: [],
 };
