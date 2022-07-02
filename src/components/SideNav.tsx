918fde77bb67bc5e8845cf120b46a5b9b7198705
Laitas <marciukash@gmail.com>
2022-07-02 14:16:13 +0300

    Update SideNav.tsx

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -6,7 +6,7 @@ const SideNav = ({ open }: { open: boolean }) => {
     <div
       className={`${
         !open && "invisible"
-      } inset-0 overflow-hidden bg-black bg-opacity-50 absolute z-10 flex justify-end mt-24`}
+      } sm:hidden inset-0 overflow-hidden bg-black bg-opacity-50 absolute z-10 flex justify-end mt-24`}
     >
       <nav
         className={`flex flex-col gap-6 p-6 bg-gray-1 max-w-[16rem] transition-all ${
